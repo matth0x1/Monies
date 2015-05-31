cdb76b10387de44acc6b0ebce7cee5428247f92e
space-rabies <sarah.giblette@gmail.com>
2015-05-31 16:03:10 +0100

    Initial commit of Person.cs

---
+++ GroupExpensesManager/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupExpensesManager
+{
+    class Person
+    {
+        public Guid Id;
+        public string Name;
+    }
+}
