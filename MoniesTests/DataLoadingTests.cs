cd54117f6b2156d6e62ccaa9e3faeaf75b2bd17b
Matt Hoskins <matth0x1@gmail.com>
2015-05-31 21:19:34 +0100

    - Added Unit Tests  - Moved DataStructure classes into folder; DataStructure.

---
+++ MoniesTests/DataLoadingTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoniesTests
+{
+    [TestClass]
+    public class DataLoadingTests
+    {
+        [TestMethod]
+        public void SimpleHappyPath()
+        {
+            string filePath = @"TestData\simpleData.xml";
+            string testData = File.ReadAllText(filePath);
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(testData), "File contents is likely empty.");
+
+            // Build DataStore.
+
+            // Calculate BalanceSheet.
+
+            // Format BalanceSheet.
+
+            // Output FormattedBalanceSheet.
+        }
+    }
+}
